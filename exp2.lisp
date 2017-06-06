98a868b48d807bb10610a0e426d2408075e85ae3
MikeH <matroxmike@icloud.com>
2017-06-06 23:59:33 +1000

    more from https://www.cs.cmu.edu/~dst/LispBook/book.pdf

--- exp2.lisp
+++ exp2.lisp
@@ -14,6 +14,37 @@
 (princ 'x= blank x)
 (mapc '(lambda (n) (plus n 1) ) nl)
 
+ (apropos "total" "USER")
+ (apropos "total" )
+ (append '(friends romans) '(and countrymen))
+ (setf who '(only the good))
+ (append who '(die young))
+ (add-to-end '(a b c) 'd)
+ (nthcdr 0 '(a b c))
+ (nthcdr 1 '(a b c))
+ (last '(all is forgiven))
+ (last '(a b c . d))
+ (setf spell '(a b r a c a d a b r a))
+ (remove 'a spell)
+ (defun beforep (x y l)
+"Returns true if X appears before Y in L"
+(member y (member x l)))
+(beforep ’not ’whom
+’(ask not for whom the bell tolls))
+(intersection '(a s d f g)  '(v w s r a))
+(setf words                              
+'((one un)
+(two deux)
+(three trois)
+(four quatre)
+(five cinq)))
+
+(assoc 'three words)
+(subst 'fred 'bill
+'(bill jones sent me an itemized
+bill for the tires))
+(sublis '((roses . violets)  (red . blue))
+'(roses are red))
 
  
  
